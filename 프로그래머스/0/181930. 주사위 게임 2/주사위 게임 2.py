9b92a6de0d60f313287b10926d72b056b6019ceb
황윤재 <hyjd5@naver.com>
2026-06-29 15:19:15 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.01 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
@@ -1,9 +1,14 @@
 def solution(a, b, c):
-    answer = 0
-    if a!=b and b!=c and a!=c :
-        answer = a+b+c
-    elif a==b==c:
-        answer=(a+b+c)*(a*a+b*b+c*c)*(a*a*a+b*b*b+c*c*c)
-    else:
-        answer=(a+b+c) * (a*a+b*b+c*c)
-    return answer
+    # 중복을 제거한 숫자의 개수를 파악합니다.
+    count = len({a, b, c})
+    
+    sum1 = a + b + c
+    sum2 = a**2 + b**2 + c**2
+    sum3 = a**3 + b**3 + c**3
+    
+    if count == 3:      # 모두 다른 경우
+        return sum1
+    elif count == 2:    # 두 개만 같은 경우
+        return sum1 * sum2
+    else:               # 모두 같은 경우 (count == 1)
+        return sum1 * sum2 * sum3
