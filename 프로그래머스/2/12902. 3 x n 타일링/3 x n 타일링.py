fddc344eaaa706cdf1c36e183d4a819f850bc2c1
황윤재 <hyjd5@naver.com>
2026-07-02 15:10:37 +0900

    [level 2] Title: 3 x n 타일링, Time: 234.40 ms, Memory: 11.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12902. 3 x n 타일링/3 x n 타일링.py
@@ -0,0 +1,19 @@
+def solution(n):
+    if n % 2 != 0:
+        return 0
+
+    MOD = 1000000007
+    
+    dp = [0] * (n + 1)
+    
+    dp[0] = 1
+    dp[2] = 3
+    
+    for i in range(4, n + 1, 2):
+        dp[i] = dp[i - 2] * 3
+        for j in range(i - 4, -1, -2):
+            dp[i] += dp[j] * 2
+        
+        dp[i] %= MOD
+        
+    return dp[n]
